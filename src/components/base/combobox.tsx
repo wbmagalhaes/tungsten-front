61c3d85f14d307181ddeb37319962f3d60599b14
William <william1504@gmail.com>
2026-02-09 10:37:05 -0300

    drag sidebar

--- src/components/base/combobox.tsx
+++ src/components/base/combobox.tsx
@@ -255,7 +255,7 @@ function ComboboxChip({
           className='-ml-1 opacity-50 hover:opacity-100'
           data-slot='combobox-chip-remove'
           render={
-            <Button variant='ghost' size='icon-xs'>
+            <Button variant='ghost' size='icon-lg'>
               <XIcon className='pointer-events-none' />
             </Button>
           }
