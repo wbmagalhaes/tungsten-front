96a086f39a839314e2666200d8b919ff3e80dffe
William <william1504@gmail.com>
2026-02-10 09:52:53 -0300

    edit profile

--- src/hooks/profile/use-edit-profile.ts
+++ src/hooks/profile/use-edit-profile.ts
@@ -1,8 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateProfile } from '@services/profile.service';
 
 export const useUpdateProfile = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updateProfile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['me'] });
+    },
   });
 };
